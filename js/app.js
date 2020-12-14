e8a8ee3ad5519068730c2e5ae1a7d109e40286c9
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2020-12-14 23:01:13 +0200

    added momements to Enemies

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function (x, y,xSpeed) {
+    this.x = x;
+    this.y = y;
+    this.xSpeed = xSpeed;
+
+   
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,10 +15,15 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
+    this.x += (this.xSpeed* Math.random())*dt;
+    if (this.x >= 500) {
+        this.x = -10;
+    }
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +34,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function (x,y) {
+    this.sprite = "images/char-cat-girl.png";
+    this.x = x;
+    this.y = y;
+};
+
+Player.prototype.update = function () {
+
+    
+};
+
+Player.prototype.render = function () {
+   ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
+}
+
+Player.prototype.handleInput = function () { };
 
 
+let player = new Player(202,400);
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemy1 = new Enemy(5, 60, 150);
 
+let enemy2 = new Enemy(200,150,150);
 
+let enemy3 = new Enemy(5,230,150);
+
+const allEnemies = [
+    enemy1,
+    enemy2,
+    enemy3
+];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
